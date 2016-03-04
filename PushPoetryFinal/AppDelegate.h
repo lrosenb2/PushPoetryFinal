1563f3fa2dac1b58b7e5e417534fea1f9a346a40
Lucas Rosenblatt <sacul123@gmail.com>
2016-03-03 20:16:43 -0500

    Initial Commit

---
+++ PushPoetryFinal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushPoetryFinal
+//
+//  Created by Lucas Rosenblatt on 3/3/16.
+//  Copyright © 2016 ZachAndLucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushPoetryFinal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushPoetryFinal
+//
+//  Created by Lucas Rosenblatt on 3/3/16.
+//  Copyright © 2016 ZachAndLucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
